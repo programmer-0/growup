2264a5f0924505abfea4e230393c5d8b114e9d99
lzc <13610226195@163.com>
2018-07-03 13:19:40 +0800

    创建成长录项目

---
+++ src/main/java/com/lzc/growup/GrowupApplication.java
@@ -0,0 +1,12 @@
+package com.lzc.growup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrowupApplication.class, args);
+    }
+}
---
+++ src/main/java/com/lzc/growup/index/IndexController.java
@@ -0,0 +1,14 @@
+package com.lzc.growup.index;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/index")
+public class IndexController {
+    @RequestMapping("/index")
+    public String index(){
+        System.out.println("首页访问！");
+        return "";
+    }
+}
